b7dcdea0c84aecda4d781b2cf61b87e153f55873
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-08-17 18:10:14 -0400

    First commit

---
+++ training/graphs.py
@@ -0,0 +1,11 @@
+class Graph:
+    def __init__(self):
+        self.vertices = {
+                            "A": {"B"},
+                            "B": {"C", "D"},
+                            "C": {"E"},
+                            "D": {"F", "G"},
+                            "E": {"C"},
+                            "F": {"C"},
+                            "G": {"A", "F"}
+                        }
